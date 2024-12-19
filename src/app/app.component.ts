0c38455c9ba65ebd97181ca07ff717c557886c7b
saud-frontend <saudtbc@gmail.com>
2024-12-19 23:23:12 +0500

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { iconSubset } from './icons/icon-subset';
     imports: [RouterOutlet]
 })
 export class AppComponent implements OnInit {
-  title = 'CoreUI Angular admin Template';
+  title = 'coreUI angular admin Template';
 
   readonly #destroyRef: DestroyRef = inject(DestroyRef);
   readonly #activatedRoute: ActivatedRoute = inject(ActivatedRoute);
