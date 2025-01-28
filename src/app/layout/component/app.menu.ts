83c36df0d4733da47a76587ae896a5620f6b5859
Waqas(Frontend) <=>
2025-01-29 02:09:41 +0500

    Adding Prime-Ng Template of Dashboard!

---
+++ src/app/layout/component/app.menu.ts
@@ -0,0 +1,119 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { AppMenuitem } from './app.menuitem';
+
+@Component({
+    selector: 'app-menu',
+    standalone: true,
+    imports: [CommonModule, AppMenuitem, RouterModule],
+    template: `<ul class="layout-menu">
+        <ng-container *ngFor="let item of model; let i = index">
+            <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true"></li>
+            <li *ngIf="item.separator" class="menu-separator"></li>
+        </ng-container>
+    </ul> `
+})
+export class AppMenu {
+    model: MenuItem[] = [];
+
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Home',
+                items: [ { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] } ]
+            },
+
+            {
+                label: 'Components',
+                items: [
+                    { label: 'Comp 1', icon: 'pi pi-fw pi-id-card'},
+                    { label: 'Comp 2', icon: 'pi pi-fw pi-check-square' },
+                    { label: 'Comp 3', icon: 'pi pi-fw pi-mobile', class: 'rotated-icon'},
+                    { label: 'Comp 4', icon: 'pi pi-fw pi-table'},
+             ]
+            },
+
+            {
+                label: 'Pages',
+                icon: 'pi pi-fw pi-briefcase',
+                routerLink: ['/pages'],
+                items: [
+                    {
+                        label: 'Auth',
+                        icon: 'pi pi-fw pi-user',
+                        items: [
+                            {
+                                label: 'Login',
+                                icon: 'pi pi-fw pi-sign-in',
+                                routerLink: ['/auth/login']
+                            },
+                            {
+                                label: 'Access Denied',
+                                icon: 'pi pi-fw pi-lock',
+                                routerLink: ['/auth/access']
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Crud',
+                        icon: 'pi pi-fw pi-pencil',
+                        routerLink: ['/pages/crud']
+                    },
+                    {
+                        label: 'Not Found',
+                        icon: 'pi pi-fw pi-exclamation-circle',
+                        routerLink: ['/pages/notfound']
+                    },
+                    {
+                        label: 'Empty',
+                        icon: 'pi pi-fw pi-circle-off',
+                        routerLink: ['/pages/empty']
+                    }
+                ]
+            },
+
+            {
+                label: 'Heading',
+                items: [
+                    {
+                        label: 'Submenu 1',
+                        icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            {
+                                label: 'Submenu 1.1',
+                                icon: 'pi pi-fw pi-bookmark',
+                                items: [
+                                    { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
+                                    { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
+                                ]
+                            },
+                            {
+                                label: 'Submenu 1.2',
+                                icon: 'pi pi-fw pi-bookmark',
+                                items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
+                            }
+                        ]
+                    },
+                ]
+            },
+
+            {
+                label: 'More',
+                items: [
+                    {
+                        label: 'Link 1',
+                        icon: 'pi pi-fw pi-book',
+                        // routerLink: ['/documentation']
+                    },
+                    {
+                        label: 'Log Out',
+                        icon: 'pi pi-fw pi-github',
+                    }
+                ]
+            }
+
+        ];
+    }
+}
---
+++ src/app/pages/auth/auth.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { Access } from './access';
+import { Login } from './login';
+
+export default [
+    { path: 'access', component: Access },
+    { path: 'login', component: Login }
+] as Routes;
