a46f51f15fdc317262fed4e29efc6c2d006d3b10
Rana <haroonrana2186123@gmail.com>
2025-03-05 21:11:17 +0500

    make environments for prod and dev

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     appName: "Salvao",
     production: true,
-    apiUrl: 'https://zenmindapi-dev.cherryberrycloud.com/api',
+    apiUrl: 'https://zenmindapi.cherryberrycloud.com/api',
   };
   
